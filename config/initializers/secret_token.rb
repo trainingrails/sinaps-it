6e57d531de8024488210a28b7d3ec5eaee7855ef
Vanya <vanya.iliyn@yahoo.com>
2011-08-26 21:52:52 +0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SinapsIt::Application.config.secret_token = '6f1ed8da2ba52fdbe50b6b34efa1ef8b0bee1d2f345fc3b024f6156779750bde04d655dc53c84e18754156310d1c03f0ad142285c1f643750b92fa1bb5fd7610'
